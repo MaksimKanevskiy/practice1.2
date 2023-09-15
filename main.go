caa6bd75ba011fd3ae3909d229e6f5dc0246f664
MaksimKanevskiy <145094580+MaksimKanevskiy@users.noreply.github.com>
2023-09-15 11:31:11 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var humanBreathing float64 = 0.5
+	var fiftyYearOak float64 = 20
+	var twentyFiveYearPoplar float64 = 32
+	var daysInYear float64 = 365
+	var breathingPerYear = humanBreathing * daysInYear
+	fmt.Println(breathingPerYear)
+	fmt.Println(math.Ceil(breathingPerYear / fiftyYearOak))
+	fmt.Println(math.Ceil(breathingPerYear / twentyFiveYearPoplar))
+}
